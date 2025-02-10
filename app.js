4e81673b755564cc812ee90cbb1e83bb99be0e42
Halconx2030 <halconx2030@gmail.com>
2025-02-10 00:05:35 -0300

    Se agrega funcion agregaramigo

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []
+
+function agregarAmigo() {
+    let input = document.getElementById("nombreAmigo");
+    let nombre = input.value.trim(); // Eliminar espacios en blanco al inicio y final.
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombre); // Agregar nombre al array
+    actualizarLista(); // Actualizar la lista en la página
+    input.value = ""; // Limpiar el campo de entrada
+
+}
+
