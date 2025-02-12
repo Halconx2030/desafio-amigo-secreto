0bdcab863842b60bacad95d959aacd4bf46c208c
Halconx2030 <halconx2030@gmail.com>
2025-02-11 22:12:20 -0300

    Se agrega una verificacion para no duplicar nombres

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []
+let amigos = [];
 
 function agregarAmigo() {
     let input = document.getElementById("amigo");
@@ -10,20 +10,23 @@ function agregarAmigo() {
         return;
     }
 
+    if (amigos.includes(nombre)) {  // Verifica si el nombre ya existe en el array
+        alert("Ese nombre ya está en la lista.");
+        return;
+    }
+
     amigos.push(nombre); // Agregar nombre al array
     actualizarLista(); // Actualizar la lista en la página
     input.value = ""; // Limpiar el campo de entrada
-
 }
 
 function actualizarLista() {
     let lista = document.getElementById("listaAmigos");
     lista.innerHTML = ""; // Limpiar lista antes de actualizar
 
-    amigos.forEach((amigo, index) => {
+    amigos.forEach((amigo) => {
         let li = document.createElement("li");
         li.textContent = amigo;
         lista.appendChild(li);
     });
-
 }
